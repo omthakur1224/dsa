c1ac3723a12b84abe5ed69099cc74027342afe63
omthakur1224 <omthakur1224@gmail.com>
2022-04-10 11:57:41 +0530

    product

--- productOfArray.js
+++ productOfArray.js
@@ -1,6 +1,21 @@
 
 
+//product of array element except current element
+// Given a array of N 32 bit integers. You need to find product of array for each i without considering ith element. where 0 <= i <= n-1 . See sample test case for better understanding.
 
+// You are not allowed to use division operator. It is given that product will fit in 32 bit integer.
+
+//  For test case 1 [1,2,3,4,5]
+
+// i = 0 ==> 2*3*4*5 = 120
+
+// i = 1 ==> 1*3*4*5 = 60
+
+// i = 3 ==> 1*2*4*5 = 40
+
+// i = 4 ==> 1*2*3*5 = 30
+
+// i = 5 ==> 1*2*3*4 = 24
 
 function product(arr,n){
     let ans=[];
