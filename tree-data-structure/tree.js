879c4be2e2383ef2617980f98a07bc94e885bd1a
omthakur1224 <omthakur1224@gmail.com>
2023-04-19 12:03:43 +0530

    tree datastructure implementation

--- tree-data-structure/tree.js
+++ tree-data-structure/tree.js
@@ -37,15 +37,37 @@ constructor(){
       if(this.root===null)return "Tree is Empty"
   }
   search(root,value){
-      if(root===null)return "not found"
-      if(root.value===value)return "Yes It's there in tree";
+      if(root===null)return false;
+      if(root.value===value)return true;
       if(root.value>value){
-          this.search(root.left,value)?"Yes Element is present": "No element is not in tree"
-          
+        return this.search(root.left,value);
       }else{
-          this.search(root.right,value)?"Yes Element is present": "No element is not in tree"
-      }
-      
+       return this.search(root.right,value);
+    }
+  }
+  preOrderTraversal(root){
+    if(root){
+        console.log(root.value);
+        this.preOrderTraversal(root.left);
+        this.preOrderTraversal(root.right)
+    }
+        
+  }
+ inOrderTraversal(root){
+    if(root){
+        this.preOrderTraversal(root.left);
+        console.log(root.value);
+        this.preOrderTraversal(root.right)
+    }
+        
+  }
+  postOrderTraversal(root){
+    if(root){
+        this.preOrderTraversal(root.left);
+        this.preOrderTraversal(root.right)
+        console.log(root.value);
+    }
+        
   }
 }
 const tree= new Tree();
@@ -54,3 +76,11 @@ tree.createNode(60)
 tree.createNode(20)
 tree.createNode(10)
 console.log("tree",tree)
+console.log("search:",tree.search(tree.root,10))
+console.log("search:",tree.search(tree.root,108))
+console.log("preOrderTraversal:")
+tree.preOrderTraversal(tree.root)
+console.log("inOrderTraversal:")
+tree.inOrderTraversal(tree.root)
+console.log("postOrderTraversal:")
+tree.postOrderTraversal(tree.root)
